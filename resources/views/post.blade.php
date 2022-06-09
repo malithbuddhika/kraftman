42237ca4c857c02b7d5e52a7cd1d9d232eba0cd3
malithbuddhika <it.malithbuddhika@gmail.com>
2022-06-09 12:31:33 +0530

    Initialized

---
+++ resources/views/post.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<title>My Blog</title>
+
+<link rel="stylesheet" href="/app.css">
+
+
+<body>
+    <article>
+        <h1><a href="post">My First Post</a></h1>
+        <p>
+            How can you start writing an article about anything when you do not have enough information about it? But fret not, this guide is here to help you understand what an article is and basically what it does.An article is a piece of writing usually intended for publication in a newspaper, magazine, or journal. It is usually written intended for a wide audience, therefore it is essential to be able to attract and retain the readers’ attention. However, styles and types for articles have different variations such as straight news, amusing feature stories, opinionated news, and so on. Regardless of the styles and types, there should always be an element of truthfulness to it; it must also contain relevant facts and information to the topic at hand.
+        </p>
+    </article>
+    <a href="/">Go Back</a>
+
+</body>
+
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<title>My Blog</title>
+
+<link rel="stylesheet" href="/app.css">
+
+
+<body>
+<article>
+    <h1><a href="posts/my-first-post">My First Post</a></h1>
+    <p>
+        How can you start writing an article about anything when you do not have enough information about it? But fret not, this guide is here to help you understand what an article is and basically what it does.An article is a piece of writing usually intended for publication in a newspaper, magazine, or journal. It is usually written intended for a wide audience, therefore it is essential to be able to attract and retain the readers’ attention. However, styles and types for articles have different variations such as straight news, amusing feature stories, opinionated news, and so on. Regardless of the styles and types, there should always be an element of truthfulness to it; it must also contain relevant facts and information to the topic at hand.
+    </p>
+</article>
+
+<article>
+    <h1><a href="posts/my-second-post">My Second Post</a></h1>
+    <p>
+        How can you start writing an article about anything when you do not have enough information about it? But fret not, this guide is here to help you understand what an article is and basically what it does.An article is a piece of writing usually intended for publication in a newspaper, magazine, or journal. It is usually written intended for a wide audience, therefore it is essential to be able to attract and retain the readers’ attention. However, styles and types for articles have different variations such as straight news, amusing feature stories, opinionated news, and so on. Regardless of the styles and types, there should always be an element of truthfulness to it; it must also contain relevant facts and information to the topic at hand.
+    </p>
+</article>
+
+<article>
+    <h1><a href="posts/my-third-post">My Third Post</a></h1>
+    <p>
+        How can you start writing an article about anything when you do not have enough information about it? But fret not, this guide is here to help you understand what an article is and basically what it does.An article is a piece of writing usually intended for publication in a newspaper, magazine, or journal. It is usually written intended for a wide audience, therefore it is essential to be able to attract and retain the readers’ attention. However, styles and types for articles have different variations such as straight news, amusing feature stories, opinionated news, and so on. Regardless of the styles and types, there should always be an element of truthfulness to it; it must also contain relevant facts and information to the topic at hand.
+    </p>
+</article>
+</body>
+
